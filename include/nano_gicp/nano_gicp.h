6a2484622c6f89380fa7b928dac9e0e4f4f4f055
gglaspell <gglaspell@gmail.com>
2025-12-18 13:36:44 -0500

    Update nano_gicp.h fix 2

--- include/nano_gicp/nano_gicp.h
+++ include/nano_gicp/nano_gicp.h
@@ -1,52 +1,7 @@
-/***********************************************************
- *                                                         *
- * Copyright (c)                                           *
- *                                                         *
- * The Verifiable & Control-Theoretic Robotics (VECTR) Lab *
- * University of California, Los Angeles                   *
- *                                                         *
- * Authors: Kenny J. Chen, Ryan Nemiroff, Brett T. Lopez   *
- * Contact: {kennyjchen, ryguyn, btlopez}@ucla.edu         *
- *                                                         *
- ***********************************************************/
-
-/***********************************************************************
- * BSD 3-Clause License
- *
- * Copyright (c) 2020, SMRT-AIST
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *************************************************************************/
-
 #pragma once
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/registration/registration.h>
@@ -56,29 +11,33 @@
 
 namespace nano_gicp {
 
-typedef std::vector<Eigen::Matrix4d, Eigen::aligned_allocator<Eigen::Matrix4d>> CovarianceList;
+template <typename PointT>
+using PointCloudPtr = typename pcl::PointCloud<PointT>::Ptr;
+template <typename PointT>
+using PointCloudConstPtr = typename pcl::PointCloud<PointT>::ConstPtr;
 
 enum class RegularizationMethod { NONE, MIN_EIG, NORMALIZED_MIN_EIG, PLANE, FROBENIUS };
 
 template<typename PointSource, typename PointTarget>
 class NanoGICP : public LsqRegistration<PointSource, PointTarget> {
-public:
-  using Scalar = float;
-  using Matrix4 = typename pcl::Registration<PointSource, PointTarget, Scalar>::Matrix4;
 
-  using PointCloudSource = typename pcl::Registration<PointSource, PointTarget, Scalar>::PointCloudSource;
+public:
+  using Scalar = typename pcl::Registration<PointSource, PointTarget, double>::Scalar;
+  using Matrix4 = typename pcl::Registration<PointSource, PointTarget, double>::Matrix4;
+  using PointCloudSource = typename pcl::Registration<PointSource, PointTarget, double>::PointCloudSource;
   using PointCloudSourcePtr = typename PointCloudSource::Ptr;
   using PointCloudSourceConstPtr = typename PointCloudSource::ConstPtr;
-
-  using PointCloudTarget = typename pcl::Registration<PointSource, PointTarget, Scalar>::PointCloudTarget;
+  using PointCloudTarget = typename pcl::Registration<PointSource, PointTarget, double>::PointCloudTarget;
   using PointCloudTargetPtr = typename PointCloudTarget::Ptr;
   using PointCloudTargetConstPtr = typename PointCloudTarget::ConstPtr;
 
 protected:
-  using pcl::Registration<PointSource, PointTarget, Scalar>::reg_name_;
-  using pcl::Registration<PointSource, PointTarget, Scalar>::input_;
-  using pcl::Registration<PointSource, PointTarget, Scalar>::target_;
-  using pcl::Registration<PointSource, PointTarget, Scalar>::final_transformation_;
+  using LsqRegistration<PointSource, PointTarget>::reg_name_;
+  using LsqRegistration<PointSource, PointTarget>::input_;
+  using LsqRegistration<PointSource, PointTarget>::target_;
+  using LsqRegistration<PointSource, PointTarget>::corr_dist_threshold_;
+  using LsqRegistration<PointSource, PointTarget>::source_covs_;
+  using LsqRegistration<PointSource, PointTarget>::target_covs_;
 
 public:
   NanoGICP();
@@ -89,47 +48,16 @@ public:
   void setMaxCorrespondenceDistance(double corr);
   void setRegularizationMethod(RegularizationMethod method);
 
-
-  /**
-   * @brief Set the weight for the photometric error term in the optimization.
-   * @param weight  The weight for the photometric error. Set to 0.0 to disable.
-   */
   void setPhotometricWeight(double weight) {
     photometric_weight_ = weight;
   }
 
-  /**
-   * @brief Sets the number of neighbors to use for spatial intensity gradient estimation.
-   * @param k Number of nearest neighbors.
-   */
   void setGradientKNeighbors(int k) {
     gradient_k_neighbors_ = k;
   }
 
-  virtual void swapSourceAndTarget() override;
-  virtual void clearSource() override;
-  virtual void clearTarget() override;
-
   virtual void setInputSource(const PointCloudSourceConstPtr& cloud) override;
-  virtual void setSourceCovariances(const std::shared_ptr<const CovarianceList>& covs);
   virtual void setInputTarget(const PointCloudTargetConstPtr& cloud) override;
-  virtual void setTargetCovariances(const std::shared_ptr<const CovarianceList>& covs);
-
-  virtual void registerInputSource(const PointCloudSourceConstPtr& cloud);
-  virtual void registerInputTarget(const PointCloudTargetConstPtr& cloud);
-
-  virtual bool calculateSourceCovariances();
-  virtual bool calculateTargetCovariances();
-
-  std::shared_ptr<const CovarianceList> getSourceCovariances() const {
-    return source_covs_;
-  }
-
-  std::shared_ptr<const CovarianceList> getTargetCovariances() const {
-    return target_covs_;
-  }
-
-  virtual void update_correspondences(const Eigen::Isometry3d& trans);
 
 protected:
   virtual void computeTransformation(PointCloudSource& output, const Matrix4& guess) override;
@@ -138,56 +66,30 @@ protected:
 
   virtual double compute_error(const Eigen::Isometry3d& trans) override;
 
+  void update_correspondences(const Eigen::Isometry3d& trans);
+
   template<typename PointT>
-  bool calculate_covariances(const typename pcl::PointCloud<PointT>::ConstPtr& cloud, const nanoflann::KdTreeFLANN<PointT>& kdtree, CovarianceList& covariances, float& density);
+  bool calculate_covariances(const typename pcl::PointCloud<PointT>::ConstPtr& cloud, const nanoflann::KdTreeFLANN<PointT>& kdtree, CovarianceList& covariances);
 
-  /**
-   * @brief Estimates the 3D spatial intensity gradient at a point in the target cloud.
-   * @param target_index  Index of the point in the target cloud.
-   * @param gradient      Output 3D gradient vector.
-   * @return              True if the gradient was estimated successfully, false otherwise.
-   */
   bool estimate_spatial_intensity_gradient(int target_index, Eigen::Vector3d& gradient) const;
-
-  /**
-   * @brief Pre-computes intensity gradients for all points in the target cloud.
-   */
+  
   void calculate_target_intensity_gradients();
-
-public:
-  std::shared_ptr<const nanoflann::KdTreeFLANN<PointSource>> source_kdtree_;
-  std::shared_ptr<const nanoflann::KdTreeFLANN<PointTarget>> target_kdtree_;
-
-  std::shared_ptr<const CovarianceList> source_covs_;
-  std::shared_ptr<const CovarianceList> target_covs_;
-
-  float source_density_;
-  float target_density_;
-
-  int num_correspondences;
 
 protected:
   int num_threads_;
   int k_correspondences_;
-  double corr_dist_threshold_;
-
   RegularizationMethod regularization_method_;
 
-  CovarianceList mahalanobis_;
-
+  std::unique_ptr<nanoflann::KdTreeFLANN<PointSource>> input_kdtree_;
+  std::unique_ptr<nanoflann::KdTreeFLANN<PointTarget>> target_kdtree_;
   std::vector<int> correspondences_;
-  std::vector<float> sq_distances_;
+  std::vector<double> sq_distances_;
+  MahalanobisList mahalanobis_;
 
-  // Weight for the photometric error term
   double photometric_weight_ = 0.0;
-
-  // Number of neighbors for gradient estimation
   int gradient_k_neighbors_ = 10;
   
-  // Pre-computed intensity gradients for the target cloud
   std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> target_intensity_gradients_;
-  
-
 };
-}  // namespace nano_gicp
 
+} // namespace nano_gicp
